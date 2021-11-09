11d5ebed12daa89a53e9088d13d2b46c935d707a
Pyae <pyaesonenyohmine143@gmail.com>
2021-11-09 07:21:02 -0500

    calculation updated

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from MarkerBuild import *
 from MarkerClass import *
 
 # using default or only camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # physical marker length in mm
 length = 66
@@ -17,19 +17,16 @@ while True:
 
     # calculate inches and write on cv2
     if marker.average_length != 0:
-        inches = int(((1536 / marker.average_length) + 1.5) * (length / 66))
+
+        inches = (1/marker.average_length) * 4900
+        inches = inches * (length/186)
+        inches = round(inches,2)
+
         cv2.putText(img, str(inches) + " in", (30, 55), cv2.QT_FONT_NORMAL, 1, (204, 209, 72), 2, cv2.LINE_AA)
 
         gap = int(marker.centroid[0] - (width / 2))
-        cv2.putText(img, str(abs(gap)), (width - 35, height - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (204, 209, 72), 1,
+        cv2.putText(img, str(gap), (width - 35, height - 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (204, 209, 72), 1,
                     cv2.LINE_AA)
-        if gap > 0:
-            cv2.putText(img, '>', (width - 25, height - 16), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (204, 209, 72), 1,
-                        cv2.LINE_AA)
-        elif gap < 0:
-            cv2.putText(img, '<', (width - 25, height - 16), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (204, 209, 72), 1,
-                        cv2.LINE_AA)
 
     cv2.imshow("Team 1493", img)
     cv2.waitKey(1)
